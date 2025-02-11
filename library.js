e8408b995f0e3000c9ee8cefc4b17f8e84e5a5d3
tetron <skim6502@gmail.com>
2025-02-11 10:13:49 -0500

    Created a visual for the library storage. Adjusted the CSS for the book visual, Added delete option, added a checkmark to toggle read for books, flexboxed the list of books visually shown

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const myLibrary = []; 
+count = 1;
 
 function Book(mytitle, myauthor, myISBN) {
     //constructor for books
@@ -11,17 +12,79 @@ function addBooktoLibrary(title, author, ISBN) {
     //take params, create a book then store it in the array
     let myBook = new Book(title, author, ISBN);
     myLibrary.push(myBook);
+    let container2 = document.querySelector("#container2");
+    let bookVersion = "book" + count.toString();
+    let bookContainer = document.createElement("div");
+    bookContainer.setAttribute("id", bookVersion);
+    bookContainer.setAttribute("style", "width: 200px; height: 300px; background-color: lightblue; margin:10px;");
+    let someTitle = document.createElement("div");
+    someTitle.textContent = title;
+    let someAuthor = document.createElement("div");
+    someAuthor.textContent = author;
+    let someISBN = document.createElement("div");
+    someISBN.textContent = ISBN;
+
+    let checkcontainer = document.createElement("div");
+    checkcontainer.setAttribute("id", "checkcontain");
+    let checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("id", "acheck");
+    let textCheck = document.createElement("label");
+    textCheck.setAttribute("for", "checkMark");
+    textCheck.textContent = "Read";
+    
+    let deleteThis = document.createElement("button");
+    deleteThis.textContent = "Delete";
+    deleteThis.addEventListener("click", () => {
+            container2.removeChild(container2.lastElementChild);
+            myLibrary.pop();
+    })
+    container2.appendChild(bookContainer);
+    bookContainer.appendChild(someTitle);
+    bookContainer.appendChild(someAuthor);
+    bookContainer.appendChild(someISBN);
+    bookContainer.appendChild(checkcontainer);
+    checkcontainer.appendChild(checkBox);
+    checkcontainer.appendChild(textCheck);
+    bookContainer.appendChild(deleteThis);
+    
 }
 
-function bookStructure(title, author, ISBN) {
+/*function bookStructure(title, author, ISBN) {
+    let bookVersion = "book" + count.toString();
+    let bookContainer = document.createElement("div");
+    bookContainer.setAttribute("id", bookVersion);
+    bookContainer.setAttribute("style", "width: 50px; height: 100px;");
+    let someTitle = document.createElement("div");
+    someTitle.textContent = title;
+    let someAuthor = document.createElement("div");
+    someAuthor.textContent = author;
+    let someISBN = document.createElement("div");
+    someISBN.textContent = ISBN;
+    let deleteThis = document.createElement("button");
+    deleteThis.textContent = "Delete";
+    deleteThis.addEventListener("click", () => {
 
-}
+            container2.removeChild(container2.lastElementChild);
+        
+    })
+    container2.appendChild(bookContainer);
+    bookContainer.appendChild(someTitle);
+    bookContainer.appendChild(someAuthor);
+    bookContainer.appendChild(someISBN);
+    bookContainer.appendChild(deleteThis);
+
+
+}*/
 
 addBooktoLibrary("Harry Potter", "JK Rowling", 13)
 let aBook = document.querySelector("#newBook");
 let container = document.querySelector("#container");
+let aDialog = document.querySelector("#aDialog");
+aDialog.setAttribute("id", "somedialog");
 console.log(aBook);
-aBook.addEventListener("click", (stat) => {
+aBook.addEventListener("click", (e) => {
+    aDialog.showModal();
     let aTitle = document.createElement("div");
     aTitle.classList.add("title")
     aTitle.textContent = "Title  ";
@@ -43,11 +106,13 @@ aBook.addEventListener("click", (stat) => {
     anISBNForm.setAttribute("type", "number");
     anISBNForm.setAttribute("id", "ISBNform");
 
-    container.appendChild(aTitle);
+    
+
+    aDialog.appendChild(aTitle);
     aTitle.appendChild(aTitleForm);
-    container.appendChild(anAuthor);
+    aDialog.appendChild(anAuthor);
     anAuthor.appendChild(anAuthorForm);
-    container.appendChild(anISBN);
+    aDialog.appendChild(anISBN);
     anISBN.appendChild(anISBNForm);
      
 
@@ -62,13 +127,13 @@ aBook.addEventListener("click", (stat) => {
         addBooktoLibrary(titleresult, authorresult, ISBNresult);
         
         console.log(myLibrary);
-        while(container.lastElementChild) {
-            container.removeChild(container.lastElementChild);
+        while(aDialog.lastElementChild) {
+            aDialog.removeChild(aDialog.lastElementChild);
         }
-     
-   
+        aDialog.close();
+
     })
-    container.appendChild(buttonclick)
+    aDialog.appendChild(buttonclick)
     
     
     
